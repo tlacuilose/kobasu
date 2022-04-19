88f83e0bf2d60d599ec4f00185bb9d9f612ba8b8
Jose Tlacuilo <tlacuilose@outlook.com>
2022-04-19 17:35:41 -0500

    Added MaterialUI Components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
+import Button from '@mui/material/Button';
+import { Box } from '@mui/system';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Button>Save</Button>
+    </Box>
   );
 }
 
