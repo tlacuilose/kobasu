7417ad4114227fc16f753394bab28a552b300cd6
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2022-06-09 20:47:16 -0500

    Connect MetaMask to join (#5)
    * Removed unused resources & Formatted code
    
    * Formatted code & Removes unused imports
    
    * Connect MetaMask to join
    
    * Safe check MetaMask

---
+++ src/presentation/global/components/topBar.tsx
@@ -0,0 +1,19 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
+interface TopBarProps {
+  text: string;
+  children: JSX.Element;
+}
+
+const TopBar = (props: TopBarProps) => (
+  <AppBar position='static'>
+    <Toolbar>
+      <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+        Kobasu {props.text}
+      </Typography>
+      {props.children}
+    </Toolbar>
+  </AppBar>
+);
+
+export default TopBar;
