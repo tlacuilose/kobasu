a674e48f33a1e564f633a5109be170a7d8c564f1
KevinTMtz <torresmtzkevin@gmail.com>
2022-06-14 03:36:44 -0500

    Add TopBar to landing page

--- src/presentation/views/landing/landing.tsx
+++ src/presentation/views/landing/landing.tsx
@@ -1,43 +1,32 @@
+import React from 'react';
 import { CssBaseline, Grid } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { ethGetAccounts } from '../../../data/web3/web3';
+
 import { ReactComponent as DriverHello } from '../../assets/driverhello.svg';
 import { ReactComponent as PassengerHello } from '../../assets/passengerhello.svg';
+import TopBar from '../../global/components/topBar';
 import LoginSection from './components/loginsection';
 
-const LandingView = () => {
-  const [isNotConnected, setIsNotConnected] = useState(true);
-
-  useEffect(() => {
-    ethGetAccounts().then(
-      (accounts) => {
-        setIsNotConnected(accounts.length === 0);
-      },
-      () => setIsNotConnected(true),
-    );
-  });
-
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Grid container>
-        <Grid item md={6} xs={12}>
-          <LoginSection
-            icon={DriverHello}
-            callToAction='Join as a driver.'
-            linkRoute='/driver'
-          />
-        </Grid>
-        <Grid item md={6} xs={12}>
-          <LoginSection
-            icon={PassengerHello}
-            callToAction='Join as a passenger.'
-            linkRoute='/passenger'
-          />
-        </Grid>
+const LandingView = () => (
+  <React.Fragment>
+    <CssBaseline />
+    <TopBar />
+    <Grid container>
+      <Grid item md={6} xs={12}>
+        <LoginSection
+          icon={DriverHello}
+          callToAction='Join as a driver.'
+          linkRoute='/driver'
+        />
       </Grid>
-    </React.Fragment>
-  );
-};
+      <Grid item md={6} xs={12}>
+        <LoginSection
+          icon={PassengerHello}
+          callToAction='Join as a passenger.'
+          linkRoute='/passenger'
+        />
+      </Grid>
+    </Grid>
+  </React.Fragment>
+);
 
 export default LandingView;
